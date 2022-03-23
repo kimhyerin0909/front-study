b68747fb881daeef48b8383796a2debe08af23ae
kimhyerin0909 <khyerin0909@gmail.com>
2022-03-23 19:38:55 +0900

    TIL : Number, Array 내장 함수

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,6 +1,2 @@
-let url = "http://naver.com";
-if(url.endsWith('.com')) {
-    console.log(true);
-} else {
-    console.log(false);
-}
+let fruits = ["Banana", "Orange", "Apple", "Mango"];
+document.write(fruits.join(" / "));
