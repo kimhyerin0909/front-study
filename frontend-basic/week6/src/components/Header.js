949c12158ef71d9c454858b54ed91b755601f05b
kimhyerin0909 <khyerin0909@gmail.com>
2022-04-19 20:15:59 +0900

    feat : header 컴포넌트 생성

---
+++ frontend-basic/week6/src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <div className='header'>
+            <img className='icon' src='./earth.png' alt='logo' />
+            <span className='title'>my travel journal.</span>
+        </div>
+    )
+}
