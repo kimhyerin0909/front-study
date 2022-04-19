08b2d487cf81663d1417f60d60d6c8bef1e1a2ff
kimhyerin0909 <khyerin0909@gmail.com>
2022-04-19 21:37:45 +0900

    feat : data 넣을 거 만드는 즁

---
+++ frontend-basic/week6/src/data.js
@@ -0,0 +1,27 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default [
+    {
+        id:1,
+        title:"Mount Fuji",
+        date:"12 Jan, 2021 - 24 Jan, 2021",
+        content:"Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        img:"Mount_Fuji.png",
+        country:"JAPAN",
+    },
+    {
+        id:2,
+        title:"Sydney Opera House",
+        date:"27 May, 2021 - 8 Jun, 2021",
+        content:"Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        img:"Mount_Fuji.png",
+        country:"AUSTRALIA",
+    },
+    {
+        id:3,
+        title:"Geirangerfjord",
+        date:"01 Oct, 2021 - 18 Nov, 2021",
+        content:"Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        img:"Mount_Fuji.png",
+        country:"NORWAY",
+    }
+]
