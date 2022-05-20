8e60d5ed8fb1b460ef56fd4fa0f8919e8dece386
kimhyerin0909 <khyerin0909@gmail.com>
2022-05-20 22:41:08 +0900

    ADD :: nextjs 배워보기

--- frontend-basic/co4ff4b9f8504c9a03077bf2c/App.js
+++ frontend-basic/co4ff4b9f8504c9a03077bf2c/App.js
@@ -2,17 +2,14 @@ import React from "react"
 import boxes from "./boxes"
 
 export default function App() {
-    /**
-     * Challenge part 1:
-     * 1. Initialize state with the default value of the
-     *    array pulled in from boxes.js
-     * 2. Map over that state array and display each one
-     *    as an empty square (black border, transparent bg color)
-     *    (Don't worry about using the "on" property yet)
-     */
+    const [squares, setSquaures] = React.useState(boxes);
+    const squareEl = squares.map(sq => (
+        <div className="box" key={sq.id}></div>
+    ))
     return (
-        <main>
-            <h1>Boxes will go here</h1>
-        </main>
-    )
+        <div>
+            hello
+            <main>{squareEl}</main>
+        </div>
+    );
 }
