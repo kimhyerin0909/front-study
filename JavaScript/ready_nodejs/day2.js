e32d2794dbaa1a7e37b660d220deb3470793ade3
kimhyerin0909 <khyerin0909@gmail.com>
2022-05-23 22:06:50 +0900

    TIL :: js 정규 표현식

---
+++ JavaScript/ready_nodejs/day2.js
@@ -0,0 +1,9 @@
+// 정규 표현식 (Regular Expression)
+
+let regexp = /World/;
+
+let str = "Hello World! world!";
+const re = /World/i;
+console.log(re.test(str));
+console.log(str.search(re));
+
---
+++ JavaScript/rememberWords.js
@@ -0,0 +1,17 @@
+function shuffleTwo(arr) {
+    let newArr = [];
+    
+    for(let i = 0; i < arr.length; i++) {
+        let randomNum = Math.floor(Math.random() * arr.length) + 1;
+    }
+}
+
+const wordList = [
+    ["agreement", "일치"],
+    ["assure", "보장하다"],
+    ["favorably", "호의적으로"],
+    ["address", "주소, 연설하다"],
+    ["capacity", "재능"],
+]
+
+shuffleTwo(wordList);
