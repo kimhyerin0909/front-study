b73f0d9635b5645134b5e8d05f9ede7180ae101b
kimhyerin0909 <khyerin0909@gmail.com>
2022-06-04 23:41:50 +0900

    FEAT :: login 기능

--- slack-clone/alecture/pages/SignUp/index.tsx
+++ slack-clone/alecture/pages/SignUp/index.tsx
@@ -4,6 +4,7 @@ import useInput from '@hooks/useInput';
 import React, { useCallback, useState } from 'react';
 import {Header, Form, Label, Input, LinkContainer, Button, Error, Success} from './style';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 const SignUp = () => {
     const [email, onChangeEmail] = useInput('');
@@ -91,7 +92,7 @@ const SignUp = () => {
       </Form>
       <LinkContainer>
         이미 회원이신가요?&nbsp;
-        <a href="/login">로그인 하러가기</a>
+        <Link to="/login">로그인 하러가기</Link>
       </LinkContainer>
     </div>
     )
